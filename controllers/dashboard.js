92a77eac8cdae3b2533b192a8e769aee16e11247
mheckner <markus.heckner@gmail.com>
2021-12-16 13:53:52 +0100

    added starter code

---
+++ controllers/dashboard.js
@@ -0,0 +1,16 @@
+const logger = require("../utils/logger.js");
+const playListCollection = require("../models/playlist-store.js");
+
+const dashboard = {
+  index(request, response) {
+    logger.info("dashboard rendering");
+    const viewData = {
+      title: "Dashboard",
+      playlists: playListCollection
+    };
+    logger.info('about to render', playListCollection);
+    response.render("dashboard", viewData);
+  }
+};
+
+module.exports = dashboard;
---
+++ models/playlist-store.js
@@ -0,0 +1,41 @@
+const logger = require("../utils/logger.js");
+
+const happySongs = {
+  title: "Happy songs",
+  songs: [
+    {
+      title: "Happy",
+      artist: "Pharrell Williams",
+    },
+    {
+      title: "Get Lucky",
+      artist: "Daft Punk",
+    },
+    {
+      title: "Don’t Worry, Be Happy",
+      artist: "Bobby McFerrin",
+    },
+  ],
+};
+
+const iconicSongs = {
+  title: "Iconic songs",
+  songs: [
+    {
+      title: "Smells Like Teen Spirit",
+      artist: "Nirvana",
+    },
+    {
+      title: "Bohemian Rhapsody",
+      artist: "Queen",
+    },
+    {
+      title: "Imagine",
+      artist: "John lennon",
+    },
+  ],
+};
+
+const playlistCollection = [happySongs, iconicSongs];
+
+module.exports = playlistCollection;
